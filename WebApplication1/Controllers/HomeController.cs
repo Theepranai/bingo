59cf951bb5555a4d5ee2de517de6ecfe0bff8416
Katon <katon44@gmail.com>
2019-03-14 03:40:56 +0700

    Set Solution and code

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Z.Expressions;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string biggo, string cal="0+0")
+        {
+            
+
+            ViewBag.result = Eval.Execute<double>(cal);
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
